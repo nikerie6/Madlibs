6ce5e4158f9fdf1c00635d9c4456f72f800988b4
nikerie6 <89383014+nikerie6@users.noreply.github.com>
2021-08-23 14:11:21 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#string concatenation
+#lonely=" bsdiwala" #some random string
+#ways to get it
+#print("Subscribe to"+ lonely)
+#print("Subscribe to".format(lonely))
+#print(f"Subscribe to{lonely}")
+adscxx=input("adjective:")
+ve1=input("verb:")
+ve2=input("verb:")
+fam_per=input("famouus person:")
+madlib=f"This is my {adscxx} beginner project \ hope you might {ve1} \ you are already {ve2} one \ i prefer to be like {fam_per}!"
+print(madlib)
